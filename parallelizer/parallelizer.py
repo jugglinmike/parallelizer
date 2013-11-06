52316ed8d37242fce5cff3798cc9126fdd99b4bd
Mike Pennisi <mike@mikepennisi.com>
2013-11-06 17:25:38 -0500

    Disable unimplemented command-line flags

--- parallelizer/parallelizer.py
+++ parallelizer/parallelizer.py
@@ -7,15 +7,15 @@ def cli():
     parser = argparse.ArgumentParser(
                         description="""spawn and benchmark multiple
                         test-running processes""")
-    parser.add_argument('-r', '--reporter', default='tap', type=str,
-                        choices=['json-stream', 'tbpl', 'tap'],
-                        dest='reporter', help='The format of the report')
+    #parser.add_argument('-r', '--reporter', default='tap', type=str,
+    #                    choices=['json-stream', 'tbpl', 'tap'],
+    #                    dest='reporter', help='The format of the report')
     parser.add_argument('-c', '--cmd', required=True, type=str,
                         dest='cmd', help='The command to execute')
-    parser.add_argument('-t', '--timeout', default=10, type=int,
-                        dest='timeout',
-                        help="""Number of seconds to wait before considering a
-                        silent process 'failed' and kiling it""")
+    #parser.add_argument('-t', '--timeout', default=10, type=int,
+    #                    dest='timeout',
+    #                    help="""Number of seconds to wait before considering a
+    #                    silent process 'failed' and kiling it""")
     parser.add_argument('files', nargs=argparse.REMAINDER, type=file)
     args = parser.parse_args()
 
