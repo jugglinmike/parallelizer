70fd646f91dafacd84f2a7c8f5289184b93810ee
Mike Pennisi <mike@mikepennisi.com>
2013-11-09 20:23:00 -0500

    Stub out benchmarking code in unit tests
    This is very brittle, but it drastically reduces the time required to
    run the tests.

--- test/spawner_test.py
+++ test/spawner_test.py
@@ -1,10 +1,24 @@
 import operator
+from nose.tools import with_setup
 
 from utils import get_fixture
 
 from parallelizer import spawner
 from parallelizer.logger import Logger
 
+run_job_orig = spawner.run_job
+def run_job_without_benchmark(*args):
+    files = len(args) and args[1]
+    if files and len(files) and 'benchmark' in files[0]:
+        return [dict(file_name=file_name, duration=1) for file_name in files]
+    return run_job_orig(*args)
+
+def mock_run_job():
+    spawner.run_job = run_job_without_benchmark
+def unmock_run_job():
+    spawner.run_job = run_job_orig
+
+@with_setup(mock_run_job, unmock_run_job)
 def basic_test():
     """The generated performance report should correctly describe test file
     name and "weight" of only those files which exited with successful error
